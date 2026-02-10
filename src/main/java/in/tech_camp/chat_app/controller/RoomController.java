c838a554d1b821d6e3b39fff2b254813b04d40bd
hahahahanr <h.nagatsuka@infomart.co.jp>
2026-02-10 09:20:43 +0900

    チャットルーム作成機能の実装

--- src/main/java/in/tech_camp/chat_app/controller/RoomController.java
+++ src/main/java/in/tech_camp/chat_app/controller/RoomController.java
@@ -1,16 +1,27 @@
 package in.tech_camp.chat_app.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import in.tech_camp.chat_app.custom_user.CustomUserDetail;
+import in.tech_camp.chat_app.entity.RoomEntity;
+import in.tech_camp.chat_app.entity.RoomUserEntity;
 import in.tech_camp.chat_app.entity.UserEntity;
 import in.tech_camp.chat_app.form.RoomForm;
+import in.tech_camp.chat_app.repository.RoomRepository;
+import in.tech_camp.chat_app.repository.RoomUserRepository;
 import in.tech_camp.chat_app.repository.UserRepository;
+import in.tech_camp.chat_app.validation.ValidationOrder;
 import lombok.AllArgsConstructor;
 
 // ルーム新規作成の操作を管理するコントローラー
@@ -18,6 +29,9 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class RoomController {
   private final UserRepository userRepository;
+
+  private final RoomRepository roomRepository;
+  private final RoomUserRepository roomUserRepository;
 
   /*・ルーム新規作成ボタンが押されたら、空の箱を作る
   　・ユーザー選択時に自分以外のユーザーを選択できるようにしている*/
@@ -27,5 +41,52 @@ public class RoomController {
     model.addAttribute("users", users);
     model.addAttribute("roomForm", new RoomForm());
     return "rooms/new";
+  }
+
+  // 新規作成して送信ボタンを押したときの動き
+  @PostMapping("/rooms")
+  // フォームでチャットルーム名を入力したとき＋ユーザーの選択（Roomの保存）
+  public String createRoom(@ModelAttribute("RoomForm") @Validated(ValidationOrder.class) RoomForm roomForm, BindingResult bindingResult, @AuthenticationPrincipal CustomUserDetail currentUser, Model model){
+    if (bindingResult.hasErrors()) {
+      List<String> errorMessages = bindingResult.getAllErrors().stream()
+                              .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                              .collect(Collectors.toList());
+      List<UserEntity> users = userRepository.findAllExcept(currentUser.getId());
+      model.addAttribute("users", users);
+      model.addAttribute("roomForm", roomForm);
+      model.addAttribute("errorMessages", errorMessages);
+      return "rooms/new";
+    }
+
+    RoomEntity roomEntity = new RoomEntity();
+    roomEntity.setName(roomForm.getName());
+    try {
+      roomRepository.insert(roomEntity);
+    } catch (Exception e) {
+      System.out.println("エラー：" + e);
+      List<UserEntity> users = userRepository.findAllExcept(currentUser.getId());
+      model.addAttribute("users", users);
+      model.addAttribute("roomForm", new RoomForm());
+      return "rooms/new";
+    }
+
+    // 選択されたユーザーをそのチャットルームに登録していく動き（RoomUserの保存）
+    List<Integer> memberIds = roomForm.getMemberIds();
+    for (Integer userId : memberIds) {
+      UserEntity userEntity = userRepository.findById(userId);
+      RoomUserEntity roomUserEntity = new RoomUserEntity();
+      roomUserEntity.setRoom(roomEntity);
+      roomUserEntity.setUser(userEntity);
+      try {
+        roomUserRepository.insert(roomUserEntity);
+      } catch (Exception e) {
+        System.out.println("エラー：" + e);
+        List<UserEntity> users = userRepository.findAllExcept(currentUser.getId());
+        model.addAttribute("users", users);
+        model.addAttribute("roomForm", new RoomForm());
+        return "rooms/new";
+      }
+    }
+    return "redirect:/";
   }
 }
